4c6a09585d9a8de3df5e412534108dc58b1d69c1
Comicome <come.dlvl@gmail.com>
2018-10-16 12:35:45 +0200

    Remove register form (WIP)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
 
-import { AlertComponent } from './_directives';
 import { AuthGuard } from './_guards';
-import { AlertService, AuthenticationService, UserService } from './_services';
+import { AuthenticationService, UserService } from './_services';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
 import { UsersComponent } from './user/users.component';
 import {FilterPipe} from "./filter.pipe";
 
@@ -28,17 +26,14 @@ import {FilterPipe} from "./filter.pipe";
     ],
     declarations: [
         AppComponent,
-        AlertComponent,
         HomeComponent,
         LoginComponent,
         UsersComponent,
         FilterPipe,
-        RegisterComponent
     ],
     providers: [
         UserService,
         AuthGuard,
-        AlertService,
         AuthenticationService,
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
