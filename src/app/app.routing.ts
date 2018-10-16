334eecbcaaa06596a088b9a3dd2b1ad6dc1fe6ba
Comicome <come.dlvl@gmail.com>
2018-10-16 12:44:21 +0200

    Remove register routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,11 @@ import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { AuthGuard } from './_guards';
 import { UsersComponent } from './user/users.component';
-import { RegisterComponent } from './register';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'profil/:id', component: UsersComponent },
-    { path: 'register', component: RegisterComponent },
     { path: 'delete/:id', component: UsersComponent },
     { path: 'add/:id', component: UsersComponent },
 
